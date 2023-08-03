a49df264ad39c01816eb76cbe82e6f1030e6fbc4
elassari <elassari19@gmail.com>
2023-08-03 16:38:30 -0700

    creat

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include main.h
+
+/**
+ * binary_to_uint - convert binary to number
+ * @b: binary
+ *
+ * Return unsigned int
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+int i;
+unsigned int nuber = 0;
+
+return (0);
+
+for (i = 0; b[i]; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+return (0);
+nuber = 2 * nuber + (b[i] - '0');
+}
+
