07d9604ef94107485c2063b975e5f87434ca5b31
Hicham El Assari <elassari19@gmail.com>
2023-08-03 23:31:32 +0100

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,9 +2,9 @@
 
 /**
  * flip_bits - flip bits
- * @n: number
- * @m: number
- * Return: number
+ * @n: num
+ * @m: num
+ * Return: int
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
