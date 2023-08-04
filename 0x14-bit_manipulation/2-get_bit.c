1d1f81d67f723d47a45d303a4c66bedf4488aded
Hicham El Assari <elassari19@gmail.com>
2023-08-04 14:33:04 +0100

    Create 2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - get bit
+ * @n: num
+ * @index: index
+ * Return: int
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+        int bit_val;
+        if (index > 63)
+                return (-1);
+
+        bit_val = (n >> index) & 1;
+        return (bit_val);
+}
