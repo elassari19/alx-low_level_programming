bb506ede822d8acd77efad5f1a6ef37ee74a30f7
Hicham El Assari <elassari19@gmail.com>
2023-08-04 11:52:17 +0100

    Update _putchar.c

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,13 +1,16 @@
-#include main.h
+#include "main.h"
+
 #include <unistd.h>
+
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - putchar
+ * @c: char
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: int
  */
+
 int _putchar(char c)
+
 {
 return (write(1, &c, 1));
 }
