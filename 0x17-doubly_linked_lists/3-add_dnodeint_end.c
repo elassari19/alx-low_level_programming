47568a0213dca811ce816c76fa50724cc3054a9e
Hicham El Assari <elassari19@gmail.com>
2023-08-17 14:38:42 +0100

    Create 3-add_dnodeint_end.c

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add dnodeint end
+ * @head: head
+ * @n: n
+ * Return: dlistint_t
+ **/
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *aux = *head;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+		new->prev = aux;
+		aux->next = new;
+	}
+	else
+	{
+		*head = new;
+		new->prev = NULL;
+	}
+
+	return (new);
+}
