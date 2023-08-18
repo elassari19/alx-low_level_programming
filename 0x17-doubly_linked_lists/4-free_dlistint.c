0a74ae4c97ac590144e8205465e9fd88fa8446ca
Hicham El Assari <elassari19@gmail.com>
2023-08-18 01:33:03 +0100

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -4,16 +4,16 @@ void free_dlistint(dlistint_t *h)
 {
 	dlistint_t *actual_node;
 	dlistint_t *next_node;
-
+	
 	if (h)
 	{
 		actual_node = h;
 		next_node = h->next;
 		while (next_node)
 		{
-			free(actual_node);
-			actual_node = next_node;
-			next_node = next_node->next;
+		free(actual_node);
+		actual_node = next_node;
+		next_node = next_node->next;
 		}
 		free(actual_node);
 	}
