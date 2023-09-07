3822a2d77d45cade7448cef605391bc0795f2dca
elassari19 <elassari19@gmail.com>
2023-09-07 18:41:44 +0100

    0x1A-hash_tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,19 +1,6 @@
 #include "hash_tables.h"
 
 /**
- * free_node - free node
- * @node: node
- * Return: void
- */
-
-void free_node(hash_node_t *node)
-{
-	free(node->key);
-	free(node->value);
-	free(node);
-}
-
-/**
  * hash_table_set - hash table set
  * @ht: ht
  * @key: key
@@ -23,44 +10,29 @@ void free_node(hash_node_t *node)
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int pointer;
-	hash_node_t *new_node, *stage;
+	unsigned long int index = 0;
+	hash_node_t *pointer = NULL, *creat_node = NULL;
 
-	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
+	if (ht == NULL || key == NULL || (strcmp(key, "") == 0))
 		return (0);
-	pointer = key_index((const unsigned char *)key, ht->size);
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
-	new_node->key = strdup((char *)key);
-	new_node->value = strdup((char *)value);
-	new_node->next = NULL;
-	if (ht->array[pointer] == NULL)
-		ht->array[pointer] = new_node;
-	else
+
+	index = key_index((unsigned char *) key, ht->size);
+	pointer = ht->array[index];
+
+	if (pointer && strcmp(key, pointer->key) == 0)
 	{
-		stage = ht->array[pointer];
-		if (strcmp(stage->key, key) == 0)
-		{
-			new_node->next = stage->next;
-			ht->array[pointer] = new_node;
-			free_node(stage);
-			return (1);
-		}
-		while (stage->next != NULL && strcmp(stage->next->key, key) != 0)
-		{ stage = stage->next;
-		}
-		if (strcmp(stage->key, key) == 0)
-		{
-			new_node->next = stage->next->next;
-			free_node(stage->next);
-			stage->next = new_node;
-		}
-		else
-		{
-			new_node->next = ht->array[pointer];
-			ht->array[pointer] = new_node;
-		}
+		free(pointer->value);
+		pointer->value = strdup(value);
+		return (1);
 	}
+
+	creat_node = malloc(sizeof(hash_node_t));
+	if (creat_node == NULL)
+		return (0);
+
+	creat_node->key = strdup(key);
+	creat_node->value = strdup(value);
+	creat_node->next = ht->array[index];
+	ht->array[index] = creat_node;
 	return (1);
 }
